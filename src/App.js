a42d1dbaf7f488f9ecb7129872fe19b43a36a325
Katelyn <developer@katelynj.co>
2023-06-09 15:57:08 -0400

    i hate render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-// import Particles from 'react-particles-js';
-import ParticlesBg from 'particles-bg'
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ParticlesBg from 'particles-bg';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 const initialState = {
@@ -15,7 +14,7 @@ const initialState = {
   imageUrl: '',
   box: {},
   route: 'signin',
-  isSignedIn: false,
+  isSignedIn: false, 
   user: {
     id: '',
     name: '',
@@ -28,29 +27,35 @@ const initialState = {
 class App extends Component {
   constructor() {
     super();
-    this.state = initialState;
-  }
+    this.state= initialState;
+    }
+
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
+    this.setState({
+      imageUrl: '',
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+        
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace =data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
+    
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
+      bottomRow: height - (clarifaiFace.bottom_row * height),
     }
   }
 
@@ -63,50 +68,88 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          input: this.state.input
-        })
-      })
-      .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
+    const imageUrl = this.state.input;
+    this.setState({ imageUrl });
+
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '74bd3ac0272a4285a419b17fc6869e0c';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'katelynjco';       
+    const APP_ID = 'smartbrain';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = 'fe995da8cb73490f8556416ecf25cea3';
+    const IMAGE_URL = imageUrl;     
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          if (result) {
+            fetch('http://localhost:3005/image', {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                id: this.state.user.id
+              })
             })
-          })
-            .then(response => response.json())
+            .then(result => result.json())
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
-            .catch(console.log)
-
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      })
-      .catch(err => console.log(err));
-  }
+            .catch(error => console.log('error', error));
+          }
+          this.displayFaceBox(this.calculateFaceLocation(result))
+        })
+        .catch(error => console.log('error', error));
+      }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState(initialState)
+      this.setState({
+        ...initialState,
+        isSignedIn: false
+      });
     } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+      this.setState({isSignedIn: true });
     }
-    this.setState({route: route});
+    this.setState({ route });
   }
+  
 
-  render() {
+  render () {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg color="#FFFFFF" type="cobweb" bg={true}/>
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         { route === 'home'
           ? <div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import './ImageLinkForm.css';
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-  return (
-    <div>
-      <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
-      </p>
-      <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
-          <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-            onClick={onButtonSubmit}
-          >Detect</button>
+    return (
+        <div>
+            <p className='f3'>
+                {'This Magic Brain will detect faces in your pictures. Give it a try!'}
+            </p>
+            <div className='center' style={{ width: '650px'}}>
+                <div className='center pa4 br3 shadow-5' style={{backgroundColor: 'linear-gradient(89deg, #FF5EDF 0%, #04C8DE)', opacity: '0.8', backgroundImage: 'linear-gradient(135deg, #04C8DE 25%, transparent 25%), linear-gradient(225deg, #04C8DE 25%, transparent 25%), linear-gradient(45deg, #04C8DE 25%, transparent 25%), linear-gradient(315deg, #04C8DE 25%, #FF5EDF 25%)', backgroundPosition: '10px 0, 10px 0, 0 0, 0 0', backgroundSize: '20px 20px', backgroundRepeat: 'repeat'}}>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                    <button 
+                    className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+                    onClick={onButtonSubmit}
+                    >Detect</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ImageLinkForm;
